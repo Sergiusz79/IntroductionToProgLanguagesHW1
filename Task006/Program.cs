dafabfefa0947aff47df17b462598747d3a421ba
Siarhei Davidzenka <mc.sergey30@gmail.com>
2023-04-11 11:44:10 +0200

    Final

--- Task006/Program.cs
+++ Task006/Program.cs
@@ -6,7 +6,7 @@ int a = Convert.ToInt32(Console.ReadLine());
 
 if (a % 2 == 0)
 {
-    Console.WriteLine("Число {a} является чётным.");
+    Console.WriteLine($"Число {a} является чётным.");
 }
 else
 {
